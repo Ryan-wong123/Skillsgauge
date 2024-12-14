e2e828a35a37846bdac88f117671212a513e59ca
zhengyang2 <67302728+zhengyang2@users.noreply.github.com>
2024-12-14 14:24:49 +0800

    loop pages

--- puppet_jobstreet.py
+++ puppet_jobstreet.py
@@ -3,6 +3,7 @@
 
 import asyncio
 from pyppeteer import launch
+import pandas
 
 
 
@@ -28,15 +29,19 @@ async def get_element_content(card, selector):
 
 
 async def job_street_scraper():
+
+    # setup section
     url = "https://sg.jobstreet.com/jobs-in-information-communication-technology"
     browser = await launch({"headless": False, "args" :['--start-maximized']}, executablePath='Win_x64_1181217_chrome-win/chrome-win/chrome.exe')
 
     page = await browser.newPage()
     await page.goto(url)
     # TODO: next time to calculate screen size and auto enter para
-
     await page.setViewport({"width": 1500, "height": 4000})
-
+    current_page = 1
+    page_count = 5
+    total_scrape_count = 0
+    total_cards = 0
 
 
     ## get HTML
@@ -53,129 +58,155 @@ async def job_street_scraper():
 
 
     print("side detected")
-    cards = await page.querySelectorAll("[data-automation='normalJob']")
-    #print(card)
 
-    #html_content = await page.evaluate('''(element) => element.outerHTML''', first_card[0])
-    #print(html_content)
-
-    # working but violate
-    #card_link = await card[0].querySelector("a")
+    while current_page <= page_count:
 
 
-
-    # TODO: save variable into pandas
-
-    card_count = 0
-    for card in cards:
-
-        # error handle urgent hiring causing link to another page
-        #TODO: future check how to solve this issue
-        if await card.querySelector("span[data-automation='urgentAdBadge']"):
-            continue
-
-        await card.click()
-        # wait for 0.5 s
-        await page.waitFor(500)
-
-        try:
-            # wait for side detail to appear
-            detail_card = await page.waitForSelector("div[data-automation='jobDetailsPage']", {"visible": True})
-
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
-            print(" detail page not open")
-            continue
+        cards = await page.querySelectorAll("[data-automation='normalJob']")
 
 
-        print("detail page open")
+        #html_content = await page.evaluate('''(element) => element.outerHTML''', first_card[0])
+        #print(html_content)
 
-        try:
-            # title section
-            title = await get_element_content(detail_card,"h1[data-automation='job-detail-title'] > a")
-            print(title)
-
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
-
-        try:
-            # link section
-            # function to extract element content
-            link_parent = await detail_card.querySelector("h1[data-automation='job-detail-title'] > a")
-
-            # return js handler and convert to py string cant merge as need to be awaited
-            link_js = await link_parent.getProperty("href")
-            link = await link_js.jsonValue()
-            print(link)
-
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
+        # working but violate
+        #card_link = await card[0].querySelector("a")
 
 
 
-        try:
-            # company name section
-            company = await get_element_content(detail_card,"span[data-automation='advertiser-name']")
-            print(company)
+        # TODO: save variable into pandas
 
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
+        card_count = 0
+        """
+        for card in cards:
 
-        try:
-            # location section
-            location = await get_element_content(detail_card,"span[data-automation='job-detail-location']")
-            print(location)
+            # error handle urgent hiring causing link to another page
+            #TODO: future check how to solve this issue
+            if await card.querySelector("span[data-automation='urgentAdBadge']"):
+                continue
 
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
+            await card.click()
+            # wait for 0.5 s
+            await page.waitFor(500)
 
+            try:
+                # wait for side detail to appear
+                detail_card = await page.waitForSelector("div[data-automation='jobDetailsPage']", {"visible": True})
 
-        try:
-            # work type section
-            work_type = await get_element_content(detail_card,"span[data-automation='job-detail-work-type']")
-            print(work_type)
-
-
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
+            except Exception as e:
+                print(f"An error occurred: {str(e)}")
+                print(" detail page not open")
+                continue
 
 
-        try:
-            # salary section
-            salary = await get_element_content(detail_card,"span[data-automation='job-detail-salary']")
-            print(salary)
+            print("detail page open")
 
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
-            salary = None
-            print(salary)
+            try:
+                # title section
+                title = await get_element_content(detail_card,"h1[data-automation='job-detail-title'] > a")
+                print(title)
 
+            except Exception as e:
+                print(f"An error occurred: {str(e)}")
 
-        try:
-            # date posted section
-            date_posted = await get_element_content(detail_card,"span._1unphw40.tcmsgw4z._1siu89c0._1siu89c1._1siu89c22._9b1ltu4._1siu89c7")
-            print(date_posted)
+            try:
+                # link section
+                # function to extract element content
+                link_parent = await detail_card.querySelector("h1[data-automation='job-detail-title'] > a")
 
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
+                # return js handler and convert to py string cant merge as need to be awaited
+                link_js = await link_parent.getProperty("href")
+                link = await link_js.jsonValue()
+                print(link)
 
-
-        try:
-            # description section
-            description = await get_element_content(detail_card,"div[data-automation='jobAdDetails']")
-            print(description)
-
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
-
-        card_count += 1
+            except Exception as e:
+                print(f"An error occurred: {str(e)}")
 
 
 
-    # scrape analysis ====================
+            try:
+                # company name section
+                company = await get_element_content(detail_card,"span[data-automation='advertiser-name']")
+                print(company)
 
-    print("total card: ", len(cards))
-    print("total scrape:", card_count)
+            except Exception as e:
+                print(f"An error occurred: {str(e)}")
+
+            try:
+                # location section
+                location = await get_element_content(detail_card,"span[data-automation='job-detail-location']")
+                print(location)
+
+            except Exception as e:
+                print(f"An error occurred: {str(e)}")
+
+
+            try:
+                # work type section
+                work_type = await get_element_content(detail_card,"span[data-automation='job-detail-work-type']")
+                print(work_type)
+
+
+            except Exception as e:
+                print(f"An error occurred: {str(e)}")
+
+
+            try:
+                # salary section
+                salary = await get_element_content(detail_card,"span[data-automation='job-detail-salary']")
+                print(salary)
+
+            except Exception as e:
+                print(f"An error occurred: {str(e)}")
+                salary = None
+                print(salary)
+
+
+            try:
+                # date posted section
+                date_posted = await get_element_content(detail_card,"span._1unphw40.tcmsgw4z._1siu89c0._1siu89c1._1siu89c22._9b1ltu4._1siu89c7")
+                print(date_posted)
+
+            except Exception as e:
+                print(f"An error occurred: {str(e)}")
+
+
+            try:
+                # description section
+                description = await get_element_content(detail_card,"div[data-automation='jobAdDetails']")
+                print(description)
+
+            except Exception as e:
+                print(f"An error occurred: {str(e)}")
+
+            card_count += 1
+        
+        """
+
+        # page scrape analysis ====================
+        print("===============================PAGE ANAlYSIS =============================================")
+        print("Total card: ", len(cards))
+        print("Total scrape:", card_count)
+        total_cards += len(cards)
+        total_scrape_count += card_count
+
+        # next page section
+
+        next_link = await page.querySelector("li > a[title='Next']")
+        await next_link.click()
+        current_page +=1
+        # wait to allow page to load
+        await page.waitFor(2000)
+        print("next paged")
+
+
+
+
+    # full scrape analysis
+    print("=============================== FULL ANAlYSIS =============================================")
+    print("total card: ", total_cards)
+    print("total scrape:", total_scrape_count)
+
+
 
 
     await browser.close()
